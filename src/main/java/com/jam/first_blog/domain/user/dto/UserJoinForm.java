d188c227c5418bfe9706ebc0cbafa226318720b8
Jaemin <Jaemin@DESKTOP-MR8U7I4>
2024-11-19 19:31:49 +0900

    회원가입 DTO에 아이디, 비밀번호 길이 제한 생성

--- src/main/java/com/jam/first_blog/domain/user/dto/UserJoinForm.java
+++ src/main/java/com/jam/first_blog/domain/user/dto/UserJoinForm.java
@@ -1,6 +1,7 @@
 package com.jam.first_blog.domain.user.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.Getter;
 
@@ -9,9 +10,11 @@ import lombok.Getter;
 public class UserJoinForm {
 	
 	@NotBlank
+	@Size(min = 4,max = 12)
 	private String username;
 	
 	@NotBlank
+	@Size(min = 6,max = 20)
 	private String password;
 	
 	@NotBlank
